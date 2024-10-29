dc30208b195172d9834f85876c1ffcbd808f7d5a
Mohammed Nazar <x3raqe@gmail.com>
2024-10-29 10:58:40 +0300

    fix: fix getMediaContent function

--- client/src/API/mainApi.js
+++ client/src/API/mainApi.js
@@ -1,7 +1,16 @@
 export async function getMediaContent() {
-  const res = await fetch(process.env.NEXT_APP_BACKEND_URL)
-  const data = await res.json()
-  return data.videos
+  const allShows = []
+
+  for (let page = 1; page <= 20; page++) {
+    const res = await fetch(
+      `${process.env.NEXT_APP_BACKEND_URL}/api/shows?page=${page}`,
+    )
+    const data = await res.json()
+
+    allShows.push(...data.shows)
+  }
+
+  return allShows
 }
 
 export async function getUsers() {
